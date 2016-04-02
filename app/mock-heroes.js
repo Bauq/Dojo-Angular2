8dc887cee9eeec7d24851a7240f6f7e9144fd4b7
Brian <brian.uribe93@gmail.com>
2016-04-02 12:36:09 -0500

    Dojo Angular2

---
+++ app/mock-heroes.js
@@ -0,0 +1,28 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Heroes;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("Heroes", Heroes = [
+                { "id": 11, "name": "Batman" },
+                { "id": 12, "name": "Flash" },
+                { "id": 13, "name": "Sueperman" },
+                { "id": 14, "name": "Iron Man" },
+                { "id": 15, "name": "Thor" },
+                { "id": 16, "name": "Dead Pool" },
+                { "id": 17, "name": "Spiderman" },
+                { "id": 18, "name": "Wonder Woman" },
+                { "id": 19, "name": "Cyborg" },
+                { "id": 20, "name": "Acuaman" }
+            ]);
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import {Hero} from './hero';
+
+export var Heroes: Hero[] = [
+	{"id":11, "name":"Batman"},
+	{"id":12, "name":"Flash"},
+	{"id":13, "name":"Sueperman"},
+	{"id":14, "name":"Iron Man"},
+	{"id":15, "name":"Thor"},
+	{"id":16, "name":"Dead Pool"},
+	{"id":17, "name":"Spiderman"},
+	{"id":18, "name":"Wonder Woman"},
+	{"id":19, "name":"Cyborg"},
+	{"id":20, "name":"Acuaman"}
+]
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
